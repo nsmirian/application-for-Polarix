84a31d3ec4df502c9dcf44660a3f2c2f9ec71109
Najmeh Mirian <42173353+nsmirian@users.noreply.github.com>
2024-02-07 22:06:32 +0100

    Add files via upload

---
+++ run_program.m
@@ -0,0 +1,7 @@
+%
+% author : Najmeh Mirian 
+% 18 jan 2024 
+% alpha version 
+display('POLARIX matlab application')
+
+ run('/System/Volumes/Data/home/ttflinac/user/mirian/application-for-Polarix-main/app4a.mlapp')
