545b011d023bfc42db61d479fb2f12ee257b93f2
Najmeh Mirian <42173353+nsmirian@users.noreply.github.com>
2024-05-13 13:02:43 +0200

    Add files via upload

---
+++ run_feedback.m
@@ -0,0 +1,9 @@
+
+
+
+% author : Najmeh Mirian 
+% 13 May 2024 
+% alpha version 
+display('POLARIX matlab application')
+
+run('/System/Volumes/Data/home/ttflinac/user/mirian/application-for-Polarix-main/app_phasefeedback.mlapp')
